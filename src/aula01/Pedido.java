9dd0636d5b0b8ce9f44c258372a3994539c11a16
AILTON PESSOA DE OLIVEIRA <2102437821@viavarejo.br>
2019-03-16 13:02:05 -0300

    Meu primeiro commit

---
+++ src/aula01/Pedido.java
@@ -0,0 +1,12 @@
+package aula01;
+
+public class Pedido {
+	private int codigo;
+	
+	public int getCodigo() {
+		return codigo;
+			}
+	public void setCodigo(int novoCodigo) {
+		codigo = novoCodigo;
+	}
+}
